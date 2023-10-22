bd209758f9b737459a7471a681f5dfe69028c9e3
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-10-22 00:21:49 -0500

    Avances

---
+++ src/main/java/dbp/techcall/TechcallApplication.java
@@ -0,0 +1,13 @@
+package dbp.techcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechcallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dbp/techcall/TechcallApplicationTests.java
@@ -0,0 +1,13 @@
+package dbp.techcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
