78c23eec04f7082f08a394e8cef44b6228c98c9d
Kayla de Vivanco <kayla.devivanco@utec.edu.pe>
2023-10-26 19:10:22 -0500

    anadiendo controlador categoria profe

--- src/main/java/dbp/techcall/course/CourseController.java
+++ src/main/java/dbp/techcall/course/CourseController.java
@@ -1,13 +1,19 @@
 package dbp.techcall.course;
+import dbp.techcall.category.Category;
+import dbp.techcall.professor.professor.infrastructure.models.Professor;
+import dbp.techcall.professor.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import dbp.techcall.course.CourseService;
 import dbp.techcall.course.CourseRepository;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/course")
@@ -17,6 +23,9 @@ public class CourseController {
 
     @Autowired
     private CourseService courseService;
+
+    @Autowired
+    private ProfessorRepository professorRepository;
 
     @GetMapping
     public ResponseEntity<List<CourseDTO>> courses() {
@@ -30,5 +39,30 @@ public class CourseController {
         return ResponseEntity.ok(courses);
 
     }
+
+    @GetMapping("/professor/{id}")
+    public ResponseEntity<List<CourseDTO>> coursesByProfessor(@PathVariable Long id){
+        Professor professor = professorRepository.findById(id);
+        if (professor == null) {
+            return ResponseEntity.status(404).body(null);
+        }
+
+        List<Course> courses = professor.getCourses();
+        List<CourseDTO> coursesDto = courses.stream()
+                .map(courseService::convertToDTO)
+                .collect(Collectors.toList());
+        return ResponseEntity.status(200).body(coursesDto);
+
+    }
+
+//    @PostMapping
+//    public ResponseEntity<CourseDTO> create(@RequestBody CourseDTO courseDTO) {
+//        Course course = courseService.convertToEntity(courseDTO);
+//        Course courseCreated = courseRepository.save(course);
+//
+//        return ResponseEntity.status(201).body(courseService.convertToDTO(courseCreated));
+//    }
+
+
 
 }
