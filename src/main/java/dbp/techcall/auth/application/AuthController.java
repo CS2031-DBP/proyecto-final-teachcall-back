d05a406bb3d3df46a413be4027dd885c20eea2d1
Mauwt <david.jacobo@utec.edu.pe>
2023-10-29 17:47:34 -0500

    CAMBIO EN LA BASE DE DATOS + Herencia funciona con el  JWT Flow + correciones en algunos Query Methods

--- src/main/java/dbp/techcall/auth/application/AuthController.java
+++ src/main/java/dbp/techcall/auth/application/AuthController.java
@@ -28,28 +28,4 @@ public class AuthController {
     public ResponseEntity<JwtRes> signin(@RequestBody LoginReq request) {
         return ResponseEntity.ok(authService.login(request));
     }
-
-
-
-
-    @PostMapping("/signup/teacher")
-    public ResponseEntity<JwtRes> registerTeacher(@RequestBody RegisterReq request) {
-        return ResponseEntity.ok(authService.registerProfessor(request));
-    }
-
-    @PostMapping("/signup/student")
-    public ResponseEntity<JwtRes> registerStudent(@RequestBody RegisterReq request) {
-        return ResponseEntity.ok(authService.registerStudent(request));
-    }
-
-
-    @PostMapping("/signin/teacher")
-    public ResponseEntity<JwtRes> signinTeacher(@RequestBody LoginReq request) {
-        return ResponseEntity.ok(authService.loginProfessor(request));
-    }
-
-    @PostMapping("/signin/student")
-    public ResponseEntity<JwtRes> signinStudent(@RequestBody LoginReq request) {
-        return ResponseEntity.ok(authService.loginStudent(request));
-    }
 }
