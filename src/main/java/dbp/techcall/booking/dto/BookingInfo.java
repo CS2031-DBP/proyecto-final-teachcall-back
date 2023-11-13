d5157ac626fce7731dee1a76d772948f1bf15d06
gortiz1234 <gustavo.ortiz@utec.edu.pe>
2023-11-13 16:01:51 -0500

    commit de booking

--- src/main/java/dbp/techcall/booking/dto/BookingInfo.java
+++ src/main/java/dbp/techcall/booking/dto/BookingInfo.java
@@ -4,18 +4,18 @@ import dbp.techcall.booking.domain.Booking;
 import dbp.techcall.meetingDetails.dto.ViewerRoomDto;
 import dbp.techcall.timeSlot.dto.StartEndDto;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
-@RequiredArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class BookingInfo {
     private StartEndDto timeSlot;
     private String professorName;
     private String professorLastname;
-    private String Title;
+    private String title;
     private ViewerRoomDto meetingDetailsLink;
 
 
@@ -26,7 +26,7 @@ public class BookingInfo {
         this.timeSlot = modelMapper.map(booking.getTimeSlot(), StartEndDto.class);
         this.professorName = booking.getProfessor().getFirstName();
         this.professorLastname = booking.getProfessor().getLastName();
-        this.Title = booking.getCourse().getTitle();
+        this.title = booking.getCourse().getTitle();
         this.meetingDetailsLink = modelMapper.map(booking.getMeetingDetails(), ViewerRoomDto.class);
     }
 }
