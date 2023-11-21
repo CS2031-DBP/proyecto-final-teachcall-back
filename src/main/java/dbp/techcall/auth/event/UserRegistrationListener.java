d4b488c236e090cc6ed9d99f3fe2645a9811c151
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-11-21 04:40:07 -0500

    Correos para Register User

---
+++ src/main/java/dbp/techcall/auth/event/UserRegistrationListener.java
@@ -0,0 +1,23 @@
+package dbp.techcall.auth.event;
+
+import dbp.techcall.booking.email.EmailService;
+import dbp.techcall.user.domain.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.EventListener;
+
+@Component
+public class UserRegistrationListener implements ApplicationListener<UserRegisteredEvent> {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Override
+    public void onApplicationEvent(UserRegisteredEvent event) {
+        Users registeredUser = event.getRegisteredUser();
+        String message = "Bienvenido a Teachcall, " + registeredUser.getFirstName() + "!";
+        emailService.sendEmailAsync(registeredUser.getEmail(), "Bienvenido a Teachcall", message);
+    }
+}
