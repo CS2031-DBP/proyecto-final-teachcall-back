90f31e8ec39f271ce85c3184e720594d8b24fd10
gortiz1234 <gustavo.ortiz@utec.edu.pe>
2023-11-23 06:38:15 -0500

    cambio de credenciales para que funcione

--- src/main/java/dbp/techcall/auth/application/AuthController.java
+++ src/main/java/dbp/techcall/auth/application/AuthController.java
@@ -6,14 +6,12 @@ import dbp.techcall.auth.dto.LoginReq;
 import dbp.techcall.auth.dto.RegisterReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//hola
+
 @RestController
 @RequestMapping("auth")
+
 public class AuthController {
 
     @Autowired
--- src/main/java/dbp/techcall/booking/application/BookingController.java
+++ src/main/java/dbp/techcall/booking/application/BookingController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookings")
+
 public class BookingController {
 
     @Autowired
